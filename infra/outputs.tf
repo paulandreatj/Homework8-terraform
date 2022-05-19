2fcc11db1bbecae94797eab351f14c1bf719a3a7
Paula Taborda <ptaborda@unal.edu.co>
2022-05-18 22:16:05 -0500

    gitignore

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -2,7 +2,3 @@ output "public_ip_address" {
   description = "public ip address"
   value       = aws_instance.ec2_instance.public_ip
 }
-
-output "eip" {
-    value = aws_eip.elastic_ip.public_ip
-}
